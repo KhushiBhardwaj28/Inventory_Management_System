49482ab70d4a4521725e1f7cb03dbfa92c8b6770
De Silva Y A S <it23551084@my.sliit.lk>
2025-12-08 09:17:20 +0530

    Add routing for Supplier and Category pages in App component

--- ims-react/src/App.js
+++ ims-react/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { ProtectedRoute, AdminRoute } from "./service/Guard";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/Login";
+import SupplierPage from "./pages/SupplierPage";
+import AddEditSupplierPage from "./pages/AddEditSupplierPage";
+import CategoryPage from "./pages/CategoryPage";
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
         <Route path="/" element={<Navigate to="/register" />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/suppliers" element={<SupplierPage />} />
+        <Route path="/add-supplier" element={<AddEditSupplierPage />} />
+        <Route path="/edit-supplier/:supplierId" element={<AddEditSupplierPage />} />
+        <Route path="/categories" element={<CategoryPage />} />
       </Routes>
     </Router>
   );
