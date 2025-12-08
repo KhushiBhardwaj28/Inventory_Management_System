92f0a7b9b624680a0d0fe5ebb2d0d55c35fcc31d
De Silva Y A S <it23551084@my.sliit.lk>
2025-12-08 08:40:10 +0530

    Add LoginPage component with authentication logic and routing

--- ims-react/src/App.js
+++ ims-react/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ProtectedRoute, AdminRoute } from "./service/Guard";
 import RegisterPage from "./pages/RegisterPage";
-
+import LoginPage from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/" element={<Navigate to="/register" />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
