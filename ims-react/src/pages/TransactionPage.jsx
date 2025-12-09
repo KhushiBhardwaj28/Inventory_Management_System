9a81dbd2122894ce535a4c4400b0f198b3c02796
De Silva Y A S <it23551084@my.sliit.lk>
2025-12-09 09:50:10 +0530

    Add search functionality and pagination to TransactionsPage component

--- ims-react/src/pages/TransactionPage.jsx
+++ ims-react/src/pages/TransactionPage.jsx
@@ -66,5 +66,62 @@ const TransactionsPage = () => {
     navigate(`/transaction/${transactionId}`);
   }
 
+  return (
+    <Layout>
+
+      {message && <p className="message">{message}</p>}
+      <div className="transactions-page">
+        <div className="transactions-header">
+            <h1>Transactions</h1>
+            <div className="transaction-search">
+                <input 
+                placeholder="Search transaction ..."
+                value={filter}
+                onChange={(e)=> setFilter(e.target.value)}
+                type="text" />
+                <button onClick={()=> handleSearch()} > Search</button>
+            </div>
+        </div>
+
+        {transactions && 
+            <table className="transactions-table">
+                <thead>
+                    <tr>
+                        <th>TYPE</th>
+                        <th>STATUS</th>
+                        <th>TOTAL PRICE</th>
+                        <th>TOTAL PRODUCTS</th>
+                        <th>DATE</th>
+                        <th>ACTIONS</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {transactions.map((transaction) => (
+                        <tr key={transaction.id}>
+                            <td>{transaction.transactionType}</td>
+                            <td>{transaction.status}</td>
+                            <td>{transaction.totalPrice}</td>
+                            <td>{transaction.totalProducts}</td>
+                            <td>{new Date(transaction.createdAt).toLocaleString()}</td>
+
+                            <td>
+                                <button onClick={()=> navigateToTransactionDetailsPage(transaction.id)}>View Details</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        }
+      </div>
+
+
+      <PaginationComponent
+      currentPage={currentPage}
+      totalPages={totalPages}
+      onPageChange={setCurrentPage}
+      />
+    </Layout>
+  );
 };
 export default TransactionsPage;
